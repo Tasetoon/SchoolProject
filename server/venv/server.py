3bb5e70a1268f42a2cba369fb9ceb4cc18bc8fb3
Philip <fsarg2007@gmail.com>
2024-03-09 00:33:22 +0300

    test page

--- server/venv/server.py
+++ server/venv/server.py
@@ -4,5 +4,11 @@ from flask import Flask, jsonify
 # app instance
 app = Flask(__name__)
 
+@app.route("/api/home", methods = ['GET'])
+def home_page():
+    return jsonify({
+        'message': 'Home page'
+    })
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
